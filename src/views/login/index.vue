ab3110d4e6719b54ef40774b69914672806881f1
bobo <syswjb@126.com>
2021-01-14 16:36:00 +0800

    用户登录组件创建-并且加入路由

---
+++ src/views/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-container">
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {
+  },
+  mounted () {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
