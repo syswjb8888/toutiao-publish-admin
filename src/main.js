00de0bef2189cf20d2f4db82d85da107d0e07268
bobo <syswjb@126.com>
2021-01-14 11:25:34 +0800

    初始化项目-调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+/**
+ * 项目启动入口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style/index.less'
+
 Vue.config.productionTip = false
 new Vue({
   router,
